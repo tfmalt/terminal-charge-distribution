54878156278915e4332f137dfc73430ac18812b0
Thomas Malt <thomas@malt.no>
2017-02-15 18:07:33 +0100

    bootstrapping repository

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+fetch('https://api.malt.no/terminal/rates.json')
+    .then( (response) => {
+      alert("response");
+      console.log("response:", response);
+    });
+
+class Tcd extends Component {
+  render() {
+    return (
+      <div className="Tcd">
+        <div className="Tcd-header">
+          <h2>Terminal Container Charge Distribution</h2>
+        </div>
+        <p className="App-intro">
+            Let's start to see some code here.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Tcd;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Tcd from './App';
+import 'whatwg-fetch';
+import './index.css';
+
+ReactDOM.render(
+  <Tcd />,
+  document.getElementById('root')
+);
