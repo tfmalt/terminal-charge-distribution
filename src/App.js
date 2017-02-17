3cbcdc1ddf419965deed05c578c069938ae1ee2d
Thomas Malt <thomas@malt.no>
2017-02-17 23:06:57 +0100

    learning my way around react one 15 minute session at a time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import React, {Component} from 'react';
-import 'whatwg-fetch';
+import React, { Component } from 'react';
+import ItemTable from './ItemTable';
 import './App.css';
 
-fetch('https://api.malt.no/terminal/rates.json').then((response) => {
-  console.log("status:", response.status);
-  console.log("response: ", response.headers.get("Content-Type"));
-  return response.json();
-}).then((json) => {
-  const list = json.map((item) => {
-    <li>{item.supplier_id + ' - ' + item.port + ' - ' + item.value}</li>
-  });
-});
-
-class ItemList extends Component {
-  render() {
-    return (
-      <ul>
-        <li>Yet another test</li>
-      </ul>
-    );
-  }
-}
 
 class App extends Component {
   render() {
@@ -30,7 +11,7 @@ class App extends Component {
           <h2>Terminal Handling Charges Distribution</h2>
         </div>
         <div id="THCDItemList">
-          <ItemList />
+          <ItemTable />
         </div>
       </div>
     );
