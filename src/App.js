cd9b6f0a2050f82cfe1b6e9cbb41987c95c536b8
Thomas Malt <thomas@malt.no>
2017-02-17 10:47:59 +0100

    Just trying to figure react out for the first time. Cleaning up.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import 'whatwg-fetch';
 import './App.css';
 
-fetch('https://api.malt.no/terminal/rates.json')
-    .then( (response) => {
-      console.log("response:", response.body);
-    });
+fetch('https://api.malt.no/terminal/rates.json').then((response) => {
+  console.log("status:", response.status);
+  console.log("response: ", response.headers.get("Content-Type"));
+  return response.json();
+}).then((json) => {
+  const list = json.map((item) => {
+    <li>{item.supplier_id + ' - ' + item.port + ' - ' + item.value}</li>
+  });
+});
 
-class Tcd extends Component {
+class ItemList extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Yet another test</li>
+      </ul>
+    );
+  }
+}
+
+class App extends Component {
   render() {
     return (
       <div className="Tcd">
         <div className="Tcd-header">
           <h2>Terminal Handling Charges Distribution</h2>
         </div>
-        <p className="App-intro">
-            Let's start to see some code here.
-        </p>
+        <div id="THCDItemList">
+          <ItemList />
+        </div>
       </div>
     );
   }
 }
 
-export default Tcd;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tcd from './App';
-import 'whatwg-fetch';
+import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <Tcd />,
+  <App />,
   document.getElementById('root')
 );
