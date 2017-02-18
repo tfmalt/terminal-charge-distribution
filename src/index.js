0c3a8caf2e4be270bbf000e7963256345c8969dd
Thomas Malt <thomas@malt.no>
2017-02-18 16:40:17 +0100

    starting on getting redux in place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
+import rates from './reducers';
 import './index.css';
+
+const store = createStore(rates);
 
 ReactDOM.render(
   <App />,
