704f535398dc31db37d047e98f63cd9c0903d9e2
Thomas Malt <thomas@malt.no>
2017-02-19 18:43:55 +0100

    Migrated to redux and redux-thunk for async code. Started using proptypes to validate props properly and got async redux to work.  Based on the official redux tutorial.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
-import ItemTable from './ItemTable';
-import './App.css';
+import React, {Component, PropTypes} from 'react';
+import ItemTable from '../components/ItemTable';
+import {connect} from 'react-redux';
+import {fetchRatesIfNeeded} from '../actions';
+import '../App.css';
 
 
 class App extends Component {
+  static propTypes = {
+    isFetching:  PropTypes.bool.isRequired,
+    items:       PropTypes.array.isRequired,
+    lastUpdated: PropTypes.number,
+    dispatch:    PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    const {dispatch} = this.props;
+    dispatch(fetchRatesIfNeeded());
+  }
+
   render() {
     return (
       <div className="Tcd">
@@ -11,11 +25,21 @@ class App extends Component {
           <h2>Terminal Handling Charges Distribution</h2>
         </div>
         <div id="THCDItemList">
-          <ItemTable />
+          <ItemTable items={this.props.items} />
         </div>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const {rates} = state;
+  const {isFetching, lastUpdated, items} = rates;
+  return {
+    isFetching,
+    lastUpdated,
+    items
+  };
+}
+
+export default connect(mapStateToProps)(App);
