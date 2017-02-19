bd184112734e524434aa8b81f6db9356d9e401c1
Thomas Malt <thomas@malt.no>
2017-02-19 18:40:20 +0100

    Updated web services helper object. cleaned up debug code

--- src/services/index.js
+++ src/services/index.js
@@ -1,31 +1,32 @@
+import 'whatwg-fetch';
+
 /**
  * Object literal helper collecting all web service API's
  */
 const apis = {
-  ratesUrl: 'https://api.malt.no/terminal/rates.json',
+  ratesUrl:  'https://api.malt.no/terminal/rates.json',
   ratesData: null,
 
   /**
    * Using fetch to get all the rates.
    */
   get rates() {
-    console.log("running fetch rates");
     if (this.ratesData instanceof Array) {
-      console.log("Found existing rates data:", this.ratesData[0]);
+      console.log('Found existing rates data:', this.ratesData[0]);
       return this.ratesData;
     }
 
     return fetch(this.ratesUrl)
       .then( (response) => {
-        console.log("got response from fetch: ", response.ok);
         if (response.ok) {
-          this.ratesData = response.json();
-          console.log("type of data: ", typeof this.ratesData);
-          console.log("is array?:", this.ratesData.toString());
-          return this.ratesData;
+          return response.json();
         }
 
         throw new Error('Got response but response was not ok');
+      })
+      .then( (items) => {
+        this.ratesData = items;
+        return items;
       })
       .catch( (error) => {
         console.error('Caught error fetching rates:', error);
