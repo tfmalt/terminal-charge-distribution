87d3a0219e5918bd1b048e979d2e707061b6b5c6
Thomas Malt <thomas@malt.no>
2017-02-19 18:46:00 +0100

    Migrated to redux and redux-thunk for async code. cleanded up actions.  Based on the official redux tutorial.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,60 @@
+import 'whatwg-fetch';
+import apis from '../services';
+
 export const GET_RATES = 'GET_RATES';
 export const GOT_RATES = 'GOT_RATES';
 
-export const getRates = (url) => ({
-  type: GET_RATES,
-  url
-});
+export const getRates = (bool) => {
+  return {
+    type:       GET_RATES,
+    isFetching: bool
+  };
+};
 
-export const gotRates = (rates) => ({
-  type: GOT_RATES,
-  rates
-});
+export const gotRates = (rates) => {
+  return {
+    type: GOT_RATES,
+    rates,
+    receivedAt: Date.now()
+  };
+};
+
+const fetchRates = () => (dispatch) => {
+  dispatch(getRates(true));
+
+  return apis.rates.then( (json) => {
+    dispatch(gotRates(json));
+  });
+};
+
+const shouldFetchRates = (state) => {
+  if (!state.rates.items) {
+    console.log("should fetch rates: no items");
+    return true;
+  }
+
+  if (!(state.rates.items instanceof Array)) {
+    console.log("should fetch rates: items not array");
+    return true;
+  }
+
+  if (state.rates.items.length < 1) {
+    console.log('should fetch rates: items is empty');
+    return true;
+  }
+
+  if (state.isFetching) {
+    console.log('should fetch rates: is already fetching');
+    return false;
+  }
+
+  console.log('should fetch rates: default clause: false');
+  return false;
+};
+
+export const fetchRatesIfNeeded = () => (dispatch, getState) => {
+  console.log('fetchRatesIfNeeded got called');
+  if (shouldFetchRates(getState())) {
+    return dispatch(fetchRates());
+  }
+};
