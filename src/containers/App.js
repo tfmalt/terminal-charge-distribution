50955c38d3a3a324d11ff38ce32d1585a2fbb1b6
Thomas Malt <thomas@malt.no>
2017-02-23 13:11:30 +0100

    Implementing UI with material-ui

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,16 @@
 import React, {Component, PropTypes} from 'react';
+import AppBar from 'material-ui/AppBar';
 import ItemTable from '../components/ItemTable';
 import {connect} from 'react-redux';
 import {fetchRatesIfNeeded} from '../actions';
 import '../App.css';
 
-
 class App extends Component {
   static propTypes = {
-    isFetching:  PropTypes.bool.isRequired,
-    items:       PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    items: PropTypes.array.isRequired,
     lastUpdated: PropTypes.number,
-    dispatch:    PropTypes.func.isRequired
+    dispatch: PropTypes.func.isRequired
   }
 
   componentDidMount() {
@@ -21,11 +21,12 @@ class App extends Component {
   render() {
     return (
       <div className="Tcd">
-        <div className="Tcd-header">
-          <h2>Terminal Handling Charges Distribution</h2>
-        </div>
+        <AppBar
+          title="Terminal Handling Charges"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
         <div id="THCDItemList">
-          <ItemTable items={this.props.items} />
+          <ItemTable items={this.props.items}/>
         </div>
       </div>
     );
@@ -35,11 +36,7 @@ class App extends Component {
 const mapStateToProps = (state) => {
   const {rates} = state;
   const {isFetching, lastUpdated, items} = rates;
-  return {
-    isFetching,
-    lastUpdated,
-    items
-  };
+  return {isFetching, lastUpdated, items};
 }
 
 export default connect(mapStateToProps)(App);
