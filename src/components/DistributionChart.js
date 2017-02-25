4523ef81e2b3d314380060cb28ad782ea3c665cf
Thomas Malt <thomas@malt.no>
2017-02-25 14:11:01 +0100

    Created a component for draing chart of rates distribution

---
+++ src/components/DistributionChart.js
@@ -0,0 +1,80 @@
+/**
+ * Using Recharts - http://recharts.org/ - to draw an area chart showing
+ * the statistical distribution of good and bad values in the data set.
+ *
+ * @link http://recharts.org/
+ * @author Thomas Malt <thomas@malt.no>
+ * @copyright 2017 (c) Thomas Malt
+ * @license MIT
+ */
+import React, {Component, PropTypes} from 'react';
+import {
+  AreaChart,
+  YAxis,
+  Area,
+  ResponsiveContainer,
+  CartesianGrid
+} from 'recharts';
+
+class DistributionChart extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    distribution: PropTypes.object.isRequired
+  }
+
+  state = {
+    data: []
+  };
+
+  componentWillReceiveProps(nextProps) {
+    console.log('DistributionChart componentWillReceiveProps:', nextProps);
+    const {distribution} = nextProps;
+    if (
+      distribution.selectedCountry !== '' &&
+      distribution.hasOwnProperty(distribution.selectedCountry) &&
+      distribution[distribution.selectedCountry].hasOwnProperty('data')
+    ) {
+      this.updateDataWithNewProps(distribution);
+    }
+  }
+
+  updateDataWithNewProps(distribution) {
+      let current     = distribution.selectedCountry;
+      let percentiles = distribution[current].data.percentiles;
+
+      let data = percentiles.map( (item) => {
+        return (item.outlier) ? {outlier: item.usd, amount: item.usd} : {amount: item.usd};
+      });
+
+      this.setState({data});
+  }
+
+  render() {
+    return (
+      <ResponsiveContainer aspect={2.32} width="100%">
+        <AreaChart
+          data={this.state.data}
+          margin={{top: 0, right: 0, left: 0, bottom: 0}}
+        >
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" horizontal={true} />
+          <Area
+            dataKey="amount"
+            strokeWidth={2}
+            type="monotone"
+            stroke='#00bcd4'
+            fill="#00bcd4"
+          />
+          <Area
+            dataKey="outlier"
+            type="monotone"
+            stroke='#ff0000'
+            fill='#ff0000'
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    );
+  }
+}
+
+export default DistributionChart;
