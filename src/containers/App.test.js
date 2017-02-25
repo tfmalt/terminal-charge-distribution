cd7dc2e8f5c0632c91b36bf944e014f10b0867bb
Thomas Malt <thomas@malt.no>
2017-02-25 14:41:08 +0100

    trying to figure out testing of redux-thunk components but skipping for now

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
-import Tcd from './App';
+import App from './App';
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Tcd />, div);
+  const store = mockStore({
+    countries: {
+      isFetching: true
+    }
+  });
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div
+  );
 });
