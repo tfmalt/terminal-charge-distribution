2aef5f9ab276c34d912f4a72465c490eae76b0eb
Thomas Malt <thomas@malt.no>
2017-02-26 17:50:03 +0100

    added react-router and implemented routing

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,29 +1,28 @@
 
 
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import MenuItem from 'material-ui/MenuItem';
 import IconMenu from 'material-ui/IconMenu';
 import IconButton from 'material-ui/IconButton';
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
-import {setCurrentAppView} from '../actions';
 
 class AppMenu extends Component {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired
-  }
+  // static propTypes = {
+  //   dispatch: PropTypes.func.isRequired
+  // }
 
-  constructor(props) {
-    super(props);
-    this.handleMenuTouchTap = this.handleMenuTouchTap.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleMenuTouchTap = this.handleMenuTouchTap.bind(this);
+  // }
 
-  handleMenuTouchTap(event, menuitem) {
-    const {value} = menuitem.props;
-    const {dispatch} = this.props;
-
-    console.log('AppMenu handleMenuTouchTap value', value);
-    dispatch(setCurrentAppView(value));
-  }
+  // handleMenuTouchTap(event, menuitem) {
+  //   const {value} = menuitem.props;
+  //   const {dispatch} = this.props;
+  //
+  //   console.log('AppMenu handleMenuTouchTap value', value);
+  //   // dispatch(setCurrentAppView(value));
+  // }
 
   render() {
     return (
@@ -35,8 +34,8 @@ class AppMenu extends Component {
         }
         onItemTouchTap={this.handleMenuTouchTap}
       >
-        <MenuItem primaryText="Distribution Chart" value="chart"/>
-        <MenuItem primaryText="Add new rate" value="add"/>
+        <MenuItem href="/" primaryText="Distribution Chart" value="chart" />
+        <MenuItem href="/add" primaryText="Add new rate" value="add" />
       </IconMenu>
     );
   }
--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import {Router, Route, browserHistory} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import reducer from './reducers';
 import App from './containers/App';
+import AddNew from './containers/AddNew';
 import './index.css';
 
 injectTapEventPlugin();
@@ -32,7 +34,10 @@ const store = createStore(reducer, applyMiddleware(...middleware));
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <App/>
+      <Router history={browserHistory}>
+        <Route path="/" component={App} />
+        <Route path="/add" component={AddNew} />
+      </Router>
     </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
